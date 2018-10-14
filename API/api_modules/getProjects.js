0888c4c8ffc7bffd5510a7142a51a17be5018961
Nima Ra <nimarasooli1@gmail.com>
2018-10-14 21:03:28 +0330

    getProjects api is working as well as possible

--- API/api_modules/getProjects.js
+++ API/api_modules/getProjects.js
@@ -1,30 +1,52 @@
 function getProjects(db, app){
-	app.post("/api/getProjects", function(req, res, next){
-  		let user_id = req.query.user_id;
-  		if (user_id != undefined){
-			db.query("SELECT `name` FROM `projects` WHERE `user_id`='" + user_id + "'", function(error, results, fields){
-     			if (error) throw error;
-       		if (results.length == 0){
-            results = {
-              status: "You have no projects"
-            }
-            res.json(results);
-          }
-          else {
-            res.json(results);
-          }
-    		});
-  		}
-  		else {
-			res.json({
-				error: "Parameters are missed"
+	app.post('/api/getProjects', (req, res, next) => {
+		var user_token = req.body.user_token ? req.body.user_token : null;
+
+		if (user_token != null) {
+			// token is passed
+			let sql = `SELECT email FROM sessions WHERE token='${user_token}'`;
+			db.query(sql ,(tokenErr, tokenRes, tokenFld) => {
+				if (tokenErr) console.log(tokenErr);
+
+				if (tokenRes[0]){
+					// token is true
+					var email = tokenRes[0].email;
+					let sql = `SELECT * FROM projects WHERE email='${email}'`;
+					db.query(sql, (projectsErr, projectsRes, projectsFld) => {
+						if (projectsErr) console.log(projectsErr);
+
+						if (projectsRes) {
+							// we have at least one project
+							var projects = [];
+
+							for (var i = 0; i < projectsRes.length; i++){
+								let item = projectsRes[i];
+								if (!projects.includes(item.name)) {
+									projects.push(item.name);
+								}
+							}
+
+							res.json({
+								projectsCount: projects.length,
+								projects: projects
+							});
+						}
+					});
+				}
+				else {
+					// token is fake
+					res.json({
+						error: 'Token is fake'
+					})
+				}
 			});
 		}
-  	});
-
-  	app.get("/api/getProjects", function(req, res, next){
-  		res.send("Documentation For Get User")
-  	});
+		else {
+			res.json({
+				error: 'Token is not passed'
+			});
+		}
+	});
 }
 
 module.exports = {
