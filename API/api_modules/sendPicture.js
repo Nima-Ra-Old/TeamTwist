b835e37329df48d90709279cd7d876b9731d8943
Nima-Ra <nimarasooli1@gmail.com>
2019-02-26 06:05:44 +0330

    now it will only send .png images

--- API/api_modules/sendPicture.js
+++ API/api_modules/sendPicture.js
@@ -1,6 +1,6 @@
 function sendPicture(app) {
   app.get('/api/photos', (req, res, next) => {
-    let picture = req.query.profilePic ? `documents/profile-images/${req.query.profilePic}.jpg` : `documents/team-images/${req.query.teamPic}.jpg`;
+    let picture = req.query.profilePic ? `documents/profile-images/${req.query.profilePic}.png` : `documents/team-images/${req.query.teamPic}.png`;
     res.sendFile(picture, {root: './'});
   });
 }
