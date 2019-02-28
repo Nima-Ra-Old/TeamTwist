3c088db2bd0b6f9142eb3e8890d7233cb9844e6a
Nima-Ra <nimarasooli1@gmail.com>
2019-02-28 13:59:29 +0330

    the show teams fine!

--- src/client/panel/teams/js/getTeams.js
+++ src/client/panel/teams/js/getTeams.js
@@ -4,24 +4,30 @@ function getCookie(name) {
   return (result === null) ? null : result[1];
 }
 
+function openTeam(i) {
+  window.location= '/team?' + i;
+}
+
+function getTeams(){
+  $.post('/api/getTeams', {user_token: token}, (data) => {
+    if (data.length == 0) {
+      $("#team-notfound").css('display', 'block');
+    } else {
+      for (let i = 0; i < data.length; i++) {
+        var element = `
+        <li class="teams-list-li" onClick="openTeam(${i})">
+        <img src="/api/photos?teamPic=${data[i].picture}">
+        <span class="teams-list-name">${data[i].name}</span>
+        </li>`
+        $("#teams-list-ul").append(element);
+      }
+      $("#teams-list").css('display', 'block');
+    }
+  });
+}
+
 var token = getCookie('token');
 
 $(document).ready( () => {
-  function getTeams(){
-    $.post('/api/getTeams', {user_token: token}, (data) => {
-      if (data.length == 0) {
-        $("#team-notfound").css('display', 'block');
-      } else {
-        for (let i = 0; i < data.length; i++) {
-            var element = `
-            <li class="teams-list-li">
-              <img src="/api/photos?teamPic=${data[i].picture}">
-            </li>`
-            $("#teams-list-ul").append(element);
-        }
-        $("#teams-list").css('display', 'block');
-      }
-    });
-  }
   getTeams();
 });
