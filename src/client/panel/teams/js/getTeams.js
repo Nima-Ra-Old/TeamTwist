4e1b1cf4d829e75e9d5f8917fe2c7222ca773daa
Nima-Ra <nimarasooli1@gmail.com>
2019-04-20 15:50:15 +0430

    finished deleting teams.

--- src/client/panel/teams/js/getTeams.js
+++ src/client/panel/teams/js/getTeams.js
@@ -1,9 +1,3 @@
-function getCookie(name) {
-  var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
-  var result = regexp.exec(document.cookie);
-  return (result === null) ? null : result[1];
-}
-
 function openTeam(i) {
   window.location= '/team?' + i;
 }
@@ -15,26 +9,9 @@ function persianTest(str) {
   else return false;
 }
 
-function deleteTeam(id) {
-  $.post('/api/deleteTeam', {user_token: token, teamId: id}, (data) => {
-    if (data.finalResult) {
-      $("#team-"+id).fadeOut('fast');
-      alert('تیم حذف شد');
-    } else {
-      alert('پیج را رفرش کنید و دوباره تلاش کنید');
-    }
-  });
-}
-
-function enableDeleting() {
-  
-}
-
 function getTeams(){
   $.post('/api/getTeams', {user_token: token}, (data) => {
-    if (data.length == 0) {
-      $("#team-notfound").css('display', 'block');
-    } else {
+    if (data[0]) {
       $(".teams-list-li").remove();
       for (let i = 0; i < data.length; i++) {
         var teamName = data[i].name;
@@ -45,19 +22,20 @@ function getTeams(){
         else languageSettings = '';
         var element = `
         <li id="team-${teamId}" class="teams-list-li" onClick="openTeam(${teamId})">
-        <img src="/api/photos?teamPic=${teamPictureId}">
+        <img src="${teamPicture}">
         <span ${languageSettings} class="teams-list-name">${teamName}</span>
-
         </li>
         `
         $("#teams-list-ul").append(element);
       }
       $("#teams-list").fadeIn('fast');
+    } else {
+      $("#teams-list").fadeOut('fast');
+      $("#team-notfound").css('display', 'block');
     }
   });
 }
 
-var token = getCookie('token');
 
 $(document).ready( () => {
   getTeams();
